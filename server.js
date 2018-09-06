768ad7a75e9c2fa8bcc6107a733763972447c4b4
Brandt Daniels <brandtdaniels@gmail.com>
2018-09-06 11:49:25 -0700

    Added projects page

--- server.js
+++ server.js
@@ -69,6 +69,14 @@ app.get('/about', (request, response) => {
 
 })
 
+app.get('/projects', (request, response) => {
+
+  response.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  })
+
+})
+
 app.get('/bad', (request, response) => {
 
   response.send({
